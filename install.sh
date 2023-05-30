a95a9df01b451aa7edf1f0fca927abcdbf4350cd
wangjiadong <wangjiadong14@huawei.com>
2023-05-30 21:10:48 +0800

    Signed-off-by: wangjiadong <wangjiadong14@huawei.com>
    change bzip2 to openEuler:bzip2

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation version 2.1
+# of the License.
+#
+# Copyright(c) 2023 Huawei Device Co., Ltd.
+
+set -e
+cd $1
+tar xvf bzip2-1.0.8.tar.gz
+cd $1/bzip2-1.0.8
+cp * ..
+exit 0
