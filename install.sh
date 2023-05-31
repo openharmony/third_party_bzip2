9f747e568a277461cd40c7fb7fe548b0d38a56d2
wangjiadong <wangjiadong14@huawei.com>
2023-05-31 19:05:46 +0800

    Signed-off-by: wangjiadong <wangjiadong14@huawei.com>
    add function

--- install.sh
+++ install.sh
@@ -10,5 +10,7 @@ set -e
 cd $1
 tar xvf bzip2-1.0.8.tar.gz
 cd $1/bzip2-1.0.8
-flock -x "README" -c "cp * .."
+flock -x "README" -c "tar xvf bzip2-1.0.8.tar.gz &&
+    cd bzip2-1.0.8 &&
+    cp * .."
 exit 0
