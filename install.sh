1add39497e564f738821c813dd6a59b4c036975c
wangjiadong <wangjiadong14@huawei.com>
2023-06-02 10:27:02 +0800

    Signed-off-by: wangjiadong <wangjiadong14@huawei.com>
    add space

--- install.sh
+++ install.sh
@@ -10,10 +10,10 @@ set -e
 cd $1
 if [ "$(uname)" == "Linux" ];then
     flock -x "README" -c "tar xvf bzip2-1.0.8.tar.gz &&
-    cd bzip2-1.0.8 &&
-    patch -p1 < $1/0002-CVE-2019-12900.patch --fuzz=0 --no-backup-if-mismatch &&
-    patch -p1 < $1/0001-add-compile-option.patch --fuzz=0 --no-backup-if-mismatch &&
-    mv * .."
+        cd bzip2-1.0.8 &&
+        patch -p1 < $1/0002-CVE-2019-12900.patch --fuzz=0 --no-backup-if-mismatch &&
+        patch -p1 < $1/0001-add-compile-option.patch --fuzz=0 --no-backup-if-mismatch &&
+        mv * .."
     exit 0
 fi
 tar xvf bzip2-1.0.8.tar.gz
